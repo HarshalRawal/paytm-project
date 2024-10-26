59a269dcbbf8b4cf793d6dc2014086457e876a58
kedar000 <140879080+kedar000@users.noreply.github.com>
2024-10-27 04:13:00 +0530

    Added : Auth using cookies

---
+++ Backend/user/src/db/prisma/migrations/20241026220658_/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "userId" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "phone" TEXT,
+    "walletId" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("userId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
--- Backend/user/src/index.ts
+++ Backend/user/src/index.ts
@@ -1,14 +1,22 @@
 import express, { Request, Response } from 'express';
-import axios from 'axios';
 import { PrismaClient } from '@prisma/client';
+import cookieParse from "cookie-parser"
+import axios from 'axios';
 import cors from 'cors';
+
+import { GenerateToken } from './functions/generateToken';
+import { logout } from './functions/logout';
+import { cookieMiddleware } from './functions/middleware';
 
 const prisma = new PrismaClient();
 const app = express();
 
 app.use(cors()); // Enable CORS for all origins
 app.use(express.json());
+app.use(cookieParse())
 
+
+app.post('/logout' , logout)
 app.post('/new-user', async (req, res) => {
   const { name, email, password, phone } = req.body;
 
@@ -29,12 +37,49 @@ app.post('/new-user', async (req, res) => {
       data: { walletId },
     });
 
-    res.status(201).json(updatedUser);
+    const token = GenerateToken(newUser.userId);
+
+
+    res.cookie('auth_token' , token);
+    console.log(token);
+    
+    res.status(201).json({updatedUser, mssg : "User signup successfully"});
+
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: "Error creating user" });
   }
 });
+
+app.post('/signin' , async(req , res)=>{
+  
+  const {email , password } = req.body;
+  const incomingUser = await prisma.user.findUnique({
+    where : {
+      email : email
+    }
+  })
+
+  if(!incomingUser){
+     res.status(404).json({mssg : "User does not exist!!  Please SignUp"})
+  }
+
+  if (incomingUser?.password != password){
+    res.status(400).json({mssg : "Incorrect Password "});
+  }
+  // @ts-ignore
+  const token = GenerateToken(incomingUser?.userId)
+
+  res.cookie('auth_token' , token);
+  console.log(token);
+  
+  res.status(200).send("User signIn succesfully")
+})
+
+
+app.post('/check' , cookieMiddleware ,async(req , res)=>{
+  res.send("hello from the check request")
+})
 
 app.listen(6001, () => {
   console.log("Server running on port 6001");
